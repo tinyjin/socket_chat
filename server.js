e9116baec0d272d9c71c9dc03985ea7b7b6c2465
tinyjin <baram991103@gmail.com>
2018-05-22 13:05:52 +0900

    implemented chat function

--- server.js
+++ server.js
@@ -10,10 +10,17 @@ app.get("/", function(req, res) {
 
 io.on('connection', function(socket) {
   console.log('a user connected');
+
   socket.on('disconnect', function() {
     console.log('user disconnected');
   });
-})
+
+  socket.on('chat message', function(msg) {
+    console.log('msg :' + msg);
+    io.emit('broadcast message', msg);
+  });
+
+});
 
 
 http.listen('8888', function() {
