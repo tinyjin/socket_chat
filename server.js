4853a14c82b6512d5767f4c2130eceae8a36a85a
tinyjin <baram991103@gmail.com>
2018-05-22 10:16:17 +0900

    create basic socket io function!

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ app.get("/", function(req, res) {
   res.sendfile("client.html");
 });
 
+io.on('connection', function(socket) {
+  console.log('a user connected');
+  socket.on('disconnect', function() {
+    console.log('user disconnected');
+  });
+})
+
+
 http.listen('8888', function() {
   console.log("start socket_chat server with 8888");
 });
